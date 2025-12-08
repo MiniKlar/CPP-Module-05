d53e2ee32384d0cfa37bfaceb626615b06fac3c0
MiniKlar <omontloris@gmail.com>
2025-12-08 02:50:55 +0100

    - Added main function to demonstrate Intern class form creation

---
+++ ex03/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/04 14:31:55 by lomont            #+#    #+#             */
+/*   Updated: 2025/12/08 02:49:09 by lomont           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include "AForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "Intern.hpp"
+
+int main(void) {
+	Intern feur;
+	AForm* formCreated;
+	formCreated = feur.makeForm("robotomy request ", "Alban");
+	delete formCreated;
+	return 0;
+}
