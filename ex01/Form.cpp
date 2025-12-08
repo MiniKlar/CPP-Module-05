c14eff6e17ad4837d38fc31220d8564afb3adf45
MiniKlar <omontloris@gmail.com>
2025-12-08 01:40:01 +0100

    - Added Form class implementation and header file with exception handling

---
+++ ex01/Form.cpp
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/05 12:03:20 by lomont            #+#    #+#             */
+/*   Updated: 2025/12/07 21:53:53 by lomont           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Form.hpp"
+#include "Bureaucrat.hpp"
+
+//Definition of GradeTooHigh & TooLow class
+
+const char* Form::GradeTooHighException::what( void ) const throw() {
+	return ("Error: Grade exceeded the maximum Grade possible (1)");
+}
+
+Form::GradeTooHighException::~GradeTooHighException( void ) throw() {
+	return ;
+}
+
+const char* Form::GradeTooLowException::what( void ) const throw() {
+	return ("Error: Grade exceeded the minimal Grade possible (150)");
+}
+
+Form::GradeTooLowException::~GradeTooLowException( void ) throw() {
+	return ;
+}
+
+// Canonical form
+
+Form::Form( void ) : _name( "Default Form"), _signed( false ), _gradeToSign( 5 ), _gradeToExec( 10 ) {
+	std::cout << "Form default constructor called" << std::endl;
+	return ;
+}
+
+Form::Form( std::string setName, int setGradeToSign, int setGradeToExec) : _name( setName ), _signed( false ), _gradeToSign( setGradeToSign ), _gradeToExec ( setGradeToExec ) {
+	std::cout << "Specific Form constructor called" << std::endl;
+	return ;
+}
+
+Form::Form( const Form& src) : _name( src._name ), _signed( src._signed ), _gradeToSign( src._gradeToSign ), _gradeToExec ( src._gradeToExec ) {
+	std::cout << "Copy Form constructor called" << std::endl;
+	return ;
+}
+
+Form& Form::operator=(const Form& other) {
+	if (&other != this) {
+		this->_signed = other._signed;
+	}
+	return *this;
+}
+
+Form::~Form( void ) {
+	std::cout << "Form destructor called" << std::endl;
+	return ;
+}
+
+//Definition of getters
+
+const std::string& Form::getName( void ) const {
+	return (_name);
+}
+
+bool Form::getSigned( void ) const {
+	return (_signed);
+}
+
+int Form::getGradeSign( void ) const {
+	return (_gradeToSign);
+}
+
+int Form::getGradeExec( void ) const {
+	return (_gradeToExec);
+}
+
+//Definition of the signing function
+
+void Form::beSigned(Bureaucrat& c) {
+	if (c.getGrade() > _gradeToSign)
+		throw GradeTooLowException();
+	_signed = true;
+	return ;
+}
+
+//Operator << overload
+
+std::ostream& operator<<(std::ostream& os, Form& f) {
+	std::string message;
+
+	message = " is signed";
+	if (!f.getSigned())
+		message = " is not signed";
+
+	os << "The form " << f.getName() << message << "." << " To be signed, the Bureaucrat needs to be grade ";
+	os << f.getGradeSign() << " and to execute it, he needs to be grade " << f.getGradeExec() << ".";
+	return (os);
+}
+
---
+++ ex01/Form.hpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/05 12:03:23 by lomont            #+#    #+#             */
+/*   Updated: 2025/12/07 23:26:21 by lomont           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#ifndef FORM_HPP
+#define	FORM_HPP
+
+#include <iostream>
+#include <string>
+#include <exception>
+
+class Bureaucrat;
+
+class Form
+{
+	private:
+		const std::string	_name;
+		bool				_signed;
+		int	const			_gradeToSign;
+		int	const			_gradeToExec;
+	public:
+		class GradeTooHighException : public std::exception {
+			public:
+				virtual const char* what() const throw();
+				virtual ~GradeTooHighException() throw();
+		};
+
+		class GradeTooLowException : public std::exception {
+			public:
+				virtual const char* what() const throw();
+				virtual ~GradeTooLowException() throw();
+		};
+
+		//Canonical form
+
+		Form( void );
+		Form( std::string setName, int setGradeToSign, int setGradeToExec );
+		Form( const Form& src);
+		Form& operator=( const Form& other);
+		~Form( void );
+
+		//Getters
+		const std::string&	getName( void ) const;
+		bool				getSigned( void ) const;
+		int					getGradeSign( void ) const;
+		int					getGradeExec( void ) const;
+
+		//Function to sign the form
+		void				beSigned(Bureaucrat& c);
+
+};
+
+std::ostream& operator<<(std::ostream& os, Form& f);
+
+#endif
