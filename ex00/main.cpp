511eef04c344e8031cb4449d88c0b96a5e156eb2
MiniKlar <omontloris@gmail.com>
2026-01-02 16:55:34 +0100

    - Refactored exception handling in main function for Bureaucrat class

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,24 +6,68 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 14:31:55 by lomont            #+#    #+#             */
-/*   Updated: 2025/12/05 00:41:09 by lomont           ###   ########.fr       */
+/*   Updated: 2026/01/02 16:54:24 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 int main(void) {
+
+	//bad grade (too low) initialization
 	try
 	{
 		Bureaucrat e("Helene", -151);
 	}
-	catch(Bureaucrat::GradeTooLowException & e)
+	catch(Bureaucrat::GradeTooHighException& e)
 	{
-		std::cerr << "catching Bureaucrat::GradeTooLowException \n  " << e.what() << std::endl;
+		std::cerr << "!! catching Bureaucrat::GradeTooHighException !!\n" << e.what() << std::endl;
+	}
+
+	//bad grade (too high) initialization
+	try
+	{
+		Bureaucrat e("Helene", 200);
+	}
+	catch(Bureaucrat::GradeTooLowException& e)
+	{
+		std::cerr << "!! catching Bureaucrat::GradeTooLowException !!\n" << e.what() << std::endl;
+	}
+
+	//good grade initialization
+	try
+	{
+		Bureaucrat e("Alban", 67);
+		std::cout << e << std::endl;
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
+
+	//Increment into bad grade
+	try
+	{
+		Bureaucrat e("IT", 1);
+		std::cout << "Voici " << e << std::endl;
+		e.IncrementGrade();
+	}
+	catch(const Bureaucrat::GradeTooHighException& e)
+	{
+		std::cerr << "!! catching Bureaucrat::GradeTooHighException !!\n" << e.what() << std::endl;
+	}
+
+	//Decrement into bad grade
+	try
+	{
+		Bureaucrat e("Kave", 150);
+		std::cout << "Voici " << e << std::endl;
+		e.DecrementGrade();
+	}
+	catch(const Bureaucrat::GradeTooLowException& e)
+	{
+		std::cerr << "!! catching Bureaucrat::GradeTooLow Exception !!\n" << e.what() << std::endl;
+	}
+
 	return (0);
 }
